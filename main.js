b4afe49d59382049886d1a18dcbfba5ceedc110a
nate-powers <73077132+nate-powers@users.noreply.github.com>
2024-10-31 08:48:37 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+<script>
+function button(button) {
+	alert("Welcome to my website")
+}
+var one = ("welcome to my website");
+function button.onMouseDown() {
+   document.getElementById("#button");
+ }
+function Button(Button) {
+	alert("Welcome to my website")
+}
+var one = ("welcome to my website");
+Function button.onMouseDown() {
+   document.getElementById("#button");
+ }
+ var = two.pop()
+parseInt
+ var class = "two"
+ var class = "one";
+ ARRAY [1]
+ ARRAY ["SUN"]["MARS"]
+ function galaxy(galaxy) {
+      SWITCH []
+      slice(1,2)
+setTimeout(function() {}, 10);
+
+ getMethod()
+ var class = "one"
+parseInt
+ alert(class)
+
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}var 
+for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
+alert(Things[])
+Things[i]
+ function galaxy(galaxy1: function() {
+
+ switch {
+ 	case 1: 
+ }
+ break;
+ 
+ function Date(setDate) {
+ 	alert(date())
+ 	// body...
+ }
+ for (var i = 0; i < Things.length; i++) {
+ 	Things[i]
+ }
+var 
+
+ </script>
+f
